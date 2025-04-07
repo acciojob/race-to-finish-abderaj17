9cadcbf6dd92e01138efe9a48ef525c5446cb1fa
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-04-07 21:26:14 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,8 @@
 window.promises = [];
 
+for(let i = 0; i < 5; i++){
+	const randomTime = Math.floor(Math.random() * 5000) + 1000;
+}
+
 // Do not change the code above this
 // add your promises to the array `promises`
