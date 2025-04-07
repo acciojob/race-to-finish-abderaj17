56e8f7409ed728b8679e6aface518fdcad729562
Adarsh Agrahari <115093974+abderaj17@users.noreply.github.com>
2025-04-07 21:27:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ window.promises = [];
 
 for(let i = 0; i < 5; i++){
 	const randomTime = Math.floor(Math.random() * 5000) + 1000;
+	setTimeout(() => {
+		resolve(`Promise ${i+1}`)
+	}, randomTime);
 }
 
 // Do not change the code above this
